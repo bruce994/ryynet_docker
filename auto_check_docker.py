1d241d8e392ab299ce80f9fcb2794915fe04f090
root <26wz.wang@gmail.com>
2017-10-18 14:46:36 +0800

    自动重启nginx中docker容器

---
+++ auto_check_docker.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+#-*- coding: UTF-8 -*-
+import sys
+import string
+import time
+#print sys.version
+import re
+import os,shutil,platform,datetime
+import calendar
+import urllib
+starttime = datetime.datetime.now()
+
+ng  = "nginx.conf"
+if os.path.isfile(ng):
+    f = open(ng, 'r')
+    content = f.read()
+    #myre = re.compile(r"server_name\s+([^;]+);[((?fastcgi_pass)\s\S)+]+fastcgi_pass\s+([^:]+):")
+    myre = re.compile(r"server_name\s+([^;]+);")
+    domain = myre.findall(content)
+
+    myre = re.compile(r"fastcgi_pass\s+([^:]+):")
+    docker = myre.findall(content)
+
+    log = "monitor_web.log"
+
+    i = 0
+    for url in domain :
+        if url == 'localhost' :
+            continue
+        goUrl = "curl -I -m 10 -o /dev/null -s -w %{http_code} "+url
+        code = os.popen(goUrl).read()
+        if code == '000' :
+            os.popen("docker restart "+docker[i]+" >> " + log )
+            os.popen('echo '+url+' >> '+log)
+            os.popen('echo '+code+' >> '+log)
+            os.popen('echo `date \'+%Y-%m-%d %H:%M:%S\'` >> '+log)
+            os.popen('echo ----------------------------- >> '+log)
+        print url + '-' + docker[i] + '-' +  code
+        i = i + 1
+
+
+endtime = datetime.datetime.now()
+print str((endtime - starttime).seconds) + ' sencond' #执行时间
