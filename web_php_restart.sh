14656368a6e6339b3e76245c078e3c4ff7241109
bruce <bruce994@users.noreply.github.com>
2018-01-25 16:52:37 +0800

    Update web_php_restart.sh

--- web_php_restart.sh
+++ web_php_restart.sh
@@ -15,13 +15,15 @@ docker run -d -p 9000:9000   --name php5.6 -v "$PWD"/php-fpm.d:/usr/local/etc/ph
 docker run -d -p 9001:9000  --name php5.6-crm0831 -v "$PWD"/php-fpm.d:/usr/local/etc/php-fpm.d  -v "$PWD":/var/www/html -v "$PWD"/php.ini:/usr/local/etc/php/php.ini  -w /var/www/html    --link mysql:mysql  -it 7543e18fff02 
 docker run -d -p 9002:9000  --name jiahe -v "$PWD"/php-fpm.d:/usr/local/etc/php-fpm.d  -v "$PWD":/var/www/html -v "$PWD"/php.ini:/usr/local/etc/php/php.ini  -w /var/www/html  --link mysql:mysql  -it 7543e18fff02 
 
+docker run -d -p 80:80 -p 443:443 --name web -v "$PWD"/nginx.conf:/etc/nginx/nginx.conf:ro -v "$PWD"/logs:/var/log/nginx  -v "$PWD":/usr/share/nginx/html:ro -v /etc/localtime:/etc/localtime:ro  --link php5.6  --link php5.6-crm0831  --link jiahe --link django-jiahe  -it nginx
+
 #django
 docker rm django-jiahe --force
 cd /home2/ryynet_docker/www/jiahe.zz.lanrenmb.com/
 #init_admin.py 初始管理员账号密码
 docker run --name django-jiahe -v "$PWD":/usr/src/app -w /usr/src/app -p 8000:8000 -d 118b37c279c0 bash -c "python manage.py makemigrations && python manage.py migrate && python init_admin.py && /usr/local/bin/gunicorn --bind 0.0.0.0:8000 mysite.wsgi:application -w 2"
 
-docker run -d -p 80:80 -p 443:443 --name web -v "$PWD"/nginx.conf:/etc/nginx/nginx.conf:ro -v "$PWD"/logs:/var/log/nginx  -v "$PWD":/usr/share/nginx/html:ro -v /etc/localtime:/etc/localtime:ro  --link php5.6  --link php5.6-crm0831  --link jiahe --link django-jiahe  -it nginx
+
 
 
 
