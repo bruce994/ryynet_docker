93ddc7c55fbc95f762db7e04963ab3827b162793
bruce <bruce994@users.noreply.github.com>
2018-02-23 18:05:09 +0800

    Update web_php_restart.sh

--- web_php_restart.sh
+++ web_php_restart.sh
@@ -15,7 +15,7 @@ docker run -d -p 9000:9000   --name php5.6 -v "$PWD"/php-fpm.d:/usr/local/etc/ph
 docker run -d -p 9001:9000  --name php5.6-crm0831 -v "$PWD"/php-fpm.d:/usr/local/etc/php-fpm.d  -v "$PWD":/var/www/html -v "$PWD"/php.ini:/usr/local/etc/php/php.ini  -w /var/www/html    --link mysql:mysql  -it 7543e18fff02 
 docker run -d -p 9002:9000  --name jiahe -v "$PWD"/php-fpm.d:/usr/local/etc/php-fpm.d  -v "$PWD":/var/www/html -v "$PWD"/php.ini:/usr/local/etc/php/php.ini  -w /var/www/html  --link mysql:mysql  -it 7543e18fff02 
 
-docker run -d -p 80:80 -p 443:443 --name web -v "$PWD"/nginx.conf:/etc/nginx/nginx.conf:ro -v "$PWD"/logs:/var/log/nginx  -v "$PWD":/usr/share/nginx/html:ro -v /etc/localtime:/etc/localtime:ro  --link php5.6  --link php5.6-crm0831  --link jiahe --link django-jiahe  -it nginx
+
 
 #django
 docker rm django-20171215 --force
@@ -24,6 +24,10 @@ cd /home2/ryynet_docker/www/20171215.tp.lanrenmb.cn/
 #docker run -d -p 8001:8000 --name django-20171215 -v "$PWD":/usr/src/app -w /usr/src/app  --link mysql:mysql  -it 88e24e8c54a6  bash -c "python manage.py makemigrations && python manage.py migrate && python init_admin.py && /usr/local/bin/gunicorn --bind 0.0.0.0:8000 mysite.wsgi:application -w 2"
 #开发环境
 docker run -d -p 8001:8000 --name django-20171215 -v "$PWD":/usr/src/app -w /usr/src/app  --link mysql:mysql  -it 88e24e8c54a6  bash -c "python manage.py runserver 0.0.0.0:8000"
+
+
+cd /home2/ryynet_docker/
+docker run -d -p 80:80 -p 443:443 --name web -v "$PWD"/nginx.conf:/etc/nginx/nginx.conf:ro -v "$PWD"/logs:/var/log/nginx  -v "$PWD":/usr/share/nginx/html:ro -v /etc/localtime:/etc/localtime:ro  --link php5.6  --link php5.6-crm0831  --link jiahe --link django-jiahe  -it nginx
 
 
 
