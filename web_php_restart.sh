742fbc6e95f1880b924837c6391b6648e6c65cc1
bruce998 <26wz.wang@gmail.com>
2020-04-12 16:16:55 +0800

    master

--- web_php_restart.sh
+++ web_php_restart.sh
@@ -7,19 +7,17 @@ service docker start
 cd /home2/ryynet_docker/
 docker rm mysql --force
 docker rm php5.6 --force
-docker rm php5.6-crm0831 --force
-docker rm jiahe --force
 docker rm web --force
 
 docker run --name mysql -d -p 3306:3306 -v "$PWD"/mysql:/var/lib/mysql -v "$PWD"/mysql.cnf:/etc/mysql/conf.d/mysql.cnf  -e MYSQL_ROOT_PASSWORD=b29VURchQW-d -e TZ='Asia/Shanghai'  -it mysql:5.7.21
 
+#php5.6
 docker run -d -p 9000:9000   --name php5.6 -v "$PWD"/php-fpm.d:/usr/local/etc/php-fpm.d  -v "$PWD":/var/www/html -v "$PWD"/php.ini:/usr/local/etc/php/php.ini  -w /var/www/html    --link mysql:mysql  -it 66856d081861
-docker run -d -p 9001:9000  --name php5.6-crm0831 -v "$PWD"/php-fpm.d:/usr/local/etc/php-fpm.d  -v "$PWD":/var/www/html -v "$PWD"/php.ini:/usr/local/etc/php/php.ini  -w /var/www/html    --link mysql:mysql  -it 66856d081861
-docker run -d -p 9002:9000  --name jiahe -v "$PWD"/php-fpm.d:/usr/local/etc/php-fpm.d  -v "$PWD":/var/www/html -v "$PWD"/php.ini:/usr/local/etc/php/php.ini  -w /var/www/html  --link mysql:mysql  -it 66856d081861
 
 #golang gin
 docker rm go-202003 --force
-docker run -d  --name go-202003  -v /home2/Guest:/go/src/app -v /home2/Guest/20200310.zz.whlanren123.top/Service/common:/go/src/common  -v /etc/localtime:/etc/localtime:ro --link mysql  -it  a59cdbf09522
+docker run -d  --name go-202003  -v /home2/ryynet_docker/www:/go/src/app -v /home2/ryynet_docker/www/vote/Service/common:/go/src/common  -v /etc/localtime:/etc/localtime:ro --link  mysql  -it a59cdbf09522  bash -c "cd src/app/vote/Service/ && fresh"
+
 
 #django
 docker rm django-20171215 --force
@@ -31,7 +29,7 @@ docker run -d -p 8001:8000 --name django-20171215 -v "$PWD":/usr/src/app -w /usr
 
 
 cd /home2/ryynet_docker/
-docker run -d -p 80:80 -p 443:443 --name web -v "$PWD"/nginx.conf:/etc/nginx/nginx.conf:ro -v "$PWD"/logs:/var/log/nginx  -v "$PWD":/usr/share/nginx/html:ro -v /etc/localtime:/etc/localtime:ro  --link php5.6  --link php5.6-crm0831  --link jiahe --link django-jiahe  -it nginx
+docker run -d -p 80:80 -p 443:443 --name web -v "$PWD"/nginx.conf:/etc/nginx/nginx.conf:ro -v "$PWD"/logs:/var/log/nginx  -v "$PWD":/usr/share/nginx/html:ro -v /etc/localtime:/etc/localtime:ro  --link php5.6  --link django-jiahe  --link go-202003  -it nginx
 
 
 
